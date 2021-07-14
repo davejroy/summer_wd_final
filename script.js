c89c9b60eed4ebac3fdc7d7c0a05fe9b4fec8a5e
David Roy <droy@worldwideservices.net>
2021-07-14 04:40:01 -0500

    retail product page - needs product selection page and script.js completed

---
+++ script.js
@@ -0,0 +1,26 @@
+function openCart() {
+
+
+}
+
+
+// when getReviews buttong is clicked scroll to bottom page orange box and resize box to fill page
+function getReviews() {
+
+
+}
+
+
+// add procuects to chopping cart modal
+function addProduct() {
+
+
+
+
+}
+
+// create functions to add items to shpping cart which is a pop up model 
+// model will add products and total
+// review hover over image and enlarge image - possibly in a modal
+
+
